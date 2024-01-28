6b7ebf315ccf9e0a440211f0a43e79fd767ebe94
Anna Aleksandrova <anyaaleksandrova1000@gmail.com>
2024-01-28 12:04:53 +0200

    Added function vivaMechMat

--- main.kt
+++ main.kt
@@ -1,8 +1,17 @@
 fun helloworld() {
     println("Hello, World")
 }
+
 fun main() {
     repeat(10) {
         helloworld()
     }
+}
+
+fun vivaMechMat() {
+    println("Viva MechMat faculty!!!")
+}
+
+fun main() {
+    vivaMechMat()
 }
